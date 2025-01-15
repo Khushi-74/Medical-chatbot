e9a39f35652f9b6cd9be9db3541f1e5b59161a66
Khushi Jain <kjain0727@gmail.com>
2025-01-15 13:22:09 +0530

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Medical-chatbot
+
+
+#how to run?
+
+step 1 create conda env 
+
+conda create -n medibot python=3.10 -y
+
+conda activate medibot
+
+step 2 install requireemnts
+
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='Generative ai project',
+    version= '0.0.0',
+    author= 'khushi',
+    author_email= 'kjain0727@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
